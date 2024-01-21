4947447788c3a28bf268ceb249f17ad9dcb62cbf
Luca Favini <luca.favini.02@gmail.com>
2024-01-21 22:03:33 +0100

    refactor(be/utils): add `common` folder

--- backend/src/common/utils.ml
+++ backend/src/common/utils.ml
@@ -1,3 +1,9 @@
+let message key value =
+  let json = `Assoc [
+    (key, `String value)
+  ] in
+  Opium.Response.of_json json |> Lwt.return
+
 let error status message detail =
   let json = `Assoc [
     ("status", `Int status);
@@ -5,3 +11,6 @@ let error status message detail =
     ("detail", `String detail)
   ] in
   Opium.Response.of_json json |> Lwt.return
+
+let simple_handler key value =
+  fun _ -> message key value
