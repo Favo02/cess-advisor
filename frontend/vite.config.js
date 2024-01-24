44046faab3b5f709d5a6addd723f0c103a644e95
Luca Favini <luca.favini.02@gmail.com>
2024-01-24 21:42:15 +0100

    build(fe): add `axios` and proxy requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
