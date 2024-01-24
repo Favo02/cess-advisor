23b275822da312f0aca9832f3e62338caa7ff99e
Luca Favini <luca.favini.02@gmail.com>
2024-01-24 23:27:39 +0100

    feat(fe/pages): add pages to routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom"
 import ProtectedRoute from "./components/ProtectedRoute"
+import LoginPage from "./components/login/LoginPage"
 import { Toaster } from "@/components/ui/toaster"
+import ProfilePage from "./components/profile/ProfilePage"
 
 const router = createBrowserRouter([
   { path: "/",              element: <Navigate to="/home" /> },
   { path: "/home",          element: <p>home</p> },
-  { path: "/login",         element: <p>login</p> },
+  { path: "/login",         element: <LoginPage /> },
   {
     path: "*",
     element: <ProtectedRoute><Outlet /></ProtectedRoute>,
     children: [
-      { path: "profile",    element: <p>profile</p> },
+      { path: "profile",    element: <ProfilePage /> },
       { path: "*",          element: <p>404 not found</p> }
     ]
   }
