73e4c3a62d5c381eb628be4b5577308f0b0c352d
Luca Favini <luca.favini.02@gmail.com>
2024-01-26 00:32:12 +0100

    feat(fe/pages): wrap pages with `Wrapper` and `Authorization`

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,21 +2,26 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom"
-import ProtectedRoute from "./components/ProtectedRoute"
+
+import Wrapper from "./components/Wrapper"
+import Authorization from "./components/Authorization"
+
 import LoginPage from "./components/login/LoginPage"
-import { Toaster } from "@/components/ui/toaster"
 import ProfilePage from "./components/profile/ProfilePage"
 
 const router = createBrowserRouter([
-  { path: "/",              element: <Navigate to="/home" /> },
-  { path: "/home",          element: <p>home</p> },
-  { path: "/login",         element: <LoginPage /> },
   {
-    path: "*",
-    element: <ProtectedRoute><Outlet /></ProtectedRoute>,
+    element: <Wrapper><Outlet /></Wrapper>,
+    errorElement: <Navigate to="/home" replace />,
     children: [
-      { path: "profile",    element: <ProfilePage /> },
-      { path: "*",          element: <p>404 not found</p> }
+      { path: "/home",          element: <p>home</p> },
+      { path: "/login",         element: <LoginPage /> },
+      {
+        element: <Authorization><Outlet /></Authorization>,
+        children: [
+          { path: "/profile",   element: <ProfilePage /> }
+        ]
+      }
     ]
   }
 ])
@@ -24,6 +29,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <Toaster />
   </React.StrictMode>
 )
