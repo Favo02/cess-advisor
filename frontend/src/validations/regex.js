6d24d84936d2f7f7f05a984573b003801f6d11d4
Luca Favini <luca.favini.02@gmail.com>
2024-01-27 21:38:20 +0100

    fix(fe/validations): update `username` regex

--- frontend/src/validations/regex.js
+++ frontend/src/validations/regex.js
@@ -1,6 +1,6 @@
 /* eslint no-useless-escape: 0 */
 
-const username = /[a-zA-Z0-9_]{4,16}$/
+const username = /^[a-zA-Z0-9_]{4,16}$/
 const usernameDesc = "4-16 characters long, alphanumeric with underscore"
 
 const password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/
