7ef6b21db81455f918c64cd156c2fa86ec968b9b
Luca Favini <luca.favini.02@gmail.com>
2024-01-28 19:02:31 +0100

    feat(fe/validations): add `regex` and `schema` for toilets

--- frontend/src/validations/regex.js
+++ frontend/src/validations/regex.js
@@ -6,7 +6,15 @@ const usernameDesc = "4-16 characters long, alphanumeric with underscore"
 const password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/
 const passwordDesc = "8-20 characters, at least one letter, one number and one special character (@$!%*#?&)"
 
+const genericName = /^[\p{Alphabetic}\p{Number} ]{6,50}$/u
+const genericNameDesc = "6-50 characters long, alphanumeric"
+
+const genericDescription = /^[\p{Alphabetic}\p{Number} ]{6,200}$/u
+const genericDescriptionDesc = "6-200 characters long, alphanumeric"
+
 export default {
   username, usernameDesc,
-  password, passwordDesc
+  password, passwordDesc,
+  genericName, genericNameDesc,
+  genericDescription, genericDescriptionDesc
 }
