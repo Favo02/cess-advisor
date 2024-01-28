c3870fa9dad83d3f2e41718a534949291ea2c69e
Luca Favini <luca.favini.02@gmail.com>
2024-01-28 13:47:09 +0100

    build(fe): customize colors, add `mui` components

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        customgreen: "#34e0a1",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
