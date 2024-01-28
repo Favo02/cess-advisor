cb7e055e6f29454fd95ad1222b1862d17f4da339
Luca Favini <luca.favini.02@gmail.com>
2024-01-28 15:41:00 +0100

    feat(be/handlers): join `toilets` and `reviews` in GET `/toilets` endpoint

--- backend/src/handlers/toilets.ml
+++ backend/src/handlers/toilets.ml
@@ -5,8 +5,15 @@ module Query = struct
   open Caqti_type.Std
 
   let get_all =
-    unit ->* tup2 (tup4 string string string int) string @@
-    "SELECT id, name, place, floor, creator FROM toilets"
+    unit ->* tup2 (tup4 string string string int) (tup4 string string (option float) int) @@
+    "SELECT
+        t.id, name, place, floor,
+        creator AS creator_id, username AS creator_name,
+        AVG(rating) AS rating, COUNT(rating) as reviews_count
+    FROM toilets t
+    INNER JOIN users u ON t.creator = u.id
+    LEFT JOIN reviews r ON t.id = r.toilet
+    GROUP BY t.id, u.username"
 
   let create =
     tup4 string string int string ->. unit @@
@@ -14,13 +21,16 @@ module Query = struct
 end
 
 module Models = struct
-  type toilet = {
-    id      : string; [@uuid]
-    name    : string; [@regex ""]
-    place   : string; [@regex ""]
-    floor   : int;    [@greater_than -3]
-    creator : string; [@uuid]
-  } [@@deriving yojson, validate]
+  type get = {
+    id            : string;
+    name          : string;
+    place         : string;
+    floor         : int;
+    creator_id    : string;
+    creator_name  : string;
+    rating        : float option;
+    reviews_count : int;
+  } [@@deriving yojson]
 
   type create = {
     name    : string; [@regex ""]
@@ -39,8 +49,8 @@ module M = Models
 let get_all _ =
   let logic () =
     let%lwt toilets = DB.collect Q.get_all () in
-    List.map (fun ((id, name, place, floor), creator) ->
-      M.yojson_of_toilet M.({ id; name; place; floor; creator; })
+    List.map (fun ((id, name, place, floor), (creator_id, creator_name, rating, reviews_count)) ->
+      M.yojson_of_get M.({ id; name; place; floor; creator_id; creator_name; rating; reviews_count; })
     ) toilets
     |> return_json_list 200
   in try
