db9045cc82acff66b2b4b168d6a316c09e6abc5b
Luca Favini <luca.favini.02@gmail.com>
2024-01-28 14:22:28 +0100

    feat(db/tables): add `reviews` table

--- database/tables.sql
+++ database/tables.sql
@@ -1,25 +1,22 @@
 CREATE TABLE users (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  username TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
+  id        uuid  PRIMARY KEY DEFAULT gen_random_uuid(),
+  username  TEXT  NOT NULL UNIQUE,
+  password  TEXT  NOT NULL
 );
 
 CREATE TABLE toilets (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  name TEXT NOT NULL,
-  place TEXT NOT NULL,
-  floor INT NOT NULL,
-  creator uuid REFERENCES users(id) NOT NULL
+  id      uuid  PRIMARY KEY DEFAULT gen_random_uuid(),
+  name    TEXT  NOT NULL,
+  place   TEXT  NOT NULL,
+  floor   INT   NOT NULL,
+  creator uuid  NOT NULL REFERENCES users(id)
 );
 
--- CREATE TABLE reviews (
---   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
---   author uuid REFERENCES users(id),
---   toilet uuid REFERENCES toilets(id),
---   description TEXT,
---   cleanliness INT,
---   toilet_paper BOOLEAN,
---   hot_water BOOLEAN,
---   soap BOOLEAN,
---   room_temperature INT
--- );
+CREATE TABLE reviews (
+  id                uuid    PRIMARY KEY DEFAULT gen_random_uuid(),
+  author            uuid    NOT NULL REFERENCES users(id),
+  date              DATE    NOT NULL,
+  toilet            uuid    NOT NULL REFERENCES toilets(id),
+  rating            INT     NOT NULL,
+  description       TEXT    NOT NULL
+);
