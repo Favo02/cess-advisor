4c5256b716806e84e4e2deb0822fc26c338ec38d
Luca Favini <luca.favini.02@gmail.com>
2024-01-28 14:14:05 +0100

    refactor(fe/services): remove `creator` in `toilets/create` service

--- frontend/src/services/toilets.js
+++ frontend/src/services/toilets.js
@@ -1,17 +1,12 @@
 import axios from "axios"
-import Cookies from "js-cookie"
 
 const baseUrl = "/api/toilets"
 
 const getAll = async () =>
   (await axios.get(`${baseUrl}`)).data
 
-const create = async (name, place, floor) => {
-  const session = Cookies.get("_session").match(/"id":"(.*?)"/)
-  const creator = session ? session[1] : null
-
-  return (await axios.post(`${baseUrl}/create`, { name, place, floor, creator })).data
-}
+const create = async (name, place, floor) =>
+  (await axios.post(`${baseUrl}/create`, { name, place, floor })).data
 
 export default {
   getAll,
