8e91843a9e85ba7b73a2bf9c5ad460c454699fa8
Luca Favini <luca.favini.02@gmail.com>
2024-04-12 23:38:16 +0200

    feat(db): update tables, add checks

--- database/tables.sql
+++ database/tables.sql
@@ -1,22 +1,29 @@
 CREATE TABLE users (
-  id        uuid  PRIMARY KEY DEFAULT gen_random_uuid(),
-  username  TEXT  NOT NULL UNIQUE,
-  password  TEXT  NOT NULL
+  id            uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  username      TEXT NOT NULL UNIQUE CHECK (username ~ '^[a-zA-Z0-9]{4,16}$'),
+  password      TEXT NOT NULL
 );
 
 CREATE TABLE toilets (
-  id      uuid  PRIMARY KEY DEFAULT gen_random_uuid(),
-  name    TEXT  NOT NULL,
-  place   TEXT  NOT NULL,
-  floor   INT   NOT NULL,
-  creator uuid  NOT NULL REFERENCES users(id)
+  id            uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  creator       uuid NOT NULL REFERENCES users(id),
+  creation      TIMESTAMP NOT NULL CHECK (creation < NOW()),
+  title         TEXT NOT NULL CHECK (title ~ '^[\t\n\x20-\xFF]{6,50}$'),
+  building      TEXT NOT NULL CHECK (building ~ '^[\t\n\x20-\xFF]{6,50}$'),
+  place         TEXT NOT NULL CHECK (place ~ '^[\t\n\x20-\xFF]{6,50}$'),
+  description   TEXT NOT NULL CHECK (description ~ '^[\t\n\x20-\xFF]{6,300}$')
 );
 
 CREATE TABLE reviews (
-  id                uuid    PRIMARY KEY DEFAULT gen_random_uuid(),
-  author            uuid    NOT NULL REFERENCES users(id),
-  date              DATE    NOT NULL,
-  toilet            uuid    NOT NULL REFERENCES toilets(id),
-  rating            INT     NOT NULL,
-  description       TEXT    NOT NULL
+  id            uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  author        uuid NOT NULL REFERENCES users(id),
+  date          TIMESTAMP NOT NULL CHECK (date < NOW()),
+  toilet        uuid NOT NULL REFERENCES toilets(id),
+  rating        INT NOT NULL CHECK (rating >= 0 AND rating <= 10),
+  description   TEXT NOT NULL CHECK (description ~ '^[\t\n\x20-\xFF]{6,300}$'),
+  paper         BOOLEAN NOT NULL,
+  soap          BOOLEAN NOT NULL,
+  dryer         BOOLEAN NOT NULL,
+  clean         INT NOT NULL CHECK (clean >= 0 AND clean <= 10),
+  temperature   INT NOT NULL CHECK (temperature >= 0 AND temperature <= 10)
 );
