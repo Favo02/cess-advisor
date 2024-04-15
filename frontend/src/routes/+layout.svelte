584dca74272d2d20fd7e9c999bcb8ad1c2d7d2dd
Luca Favini <luca.favini.02@gmail.com>
2024-04-15 22:37:45 +0200

    build(fe): basic sveltekit frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+  <a href="/">home</a>
+  <a href="/profile">profile</a>
+  <a href="/login">login</a>
+</nav>
+
+<slot />
---
+++ frontend/src/routes/profile/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import axios from "axios";
+
+  let promise = axios.get("/api/users/me");
+</script>
+
+<h1>Profile</h1>
+
+{#await promise}
+  <p>Loading...</p>
+{:then profile}
+  <p>Profile: {JSON.stringify(profile.data)}</p>
+{:catch error}
+  <p style="color: red">Error: {JSON.stringify(error.response.data)}</p>
+{/await}
