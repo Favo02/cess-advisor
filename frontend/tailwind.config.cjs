20c7a1680456b93f650a332e2ac4e6214c979d95
Luca Favini <luca.favini.02@gmail.com>
2024-04-17 00:07:46 +0200

    build(fe): add `flowbite` icons and blocks

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,7 +1,12 @@
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte-blocks/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}',
+  ],
 
-  plugins: [require('flowbite/plugin')],
+  plugins: [require('flowbite/plugin'), require('flowbite-typography')],
 
   darkMode: 'class',
 
