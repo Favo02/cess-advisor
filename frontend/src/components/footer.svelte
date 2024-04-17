28d4ad40aaa36facb5f5c296f886fc8db7057394
Luca Favini <luca.favini.02@gmail.com>
2024-04-17 14:44:36 +0200

    build(fe): remove `flowbite` components

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,18 +1,1 @@
-<script>
-  import { Footer, FooterBrand, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
-</script>
-
-<Footer footerType="socialmedia">
-  <div class="mx-auto max-w-screen-xl text-center mt-12">
-    <FooterBrand href="/" src="/logo.png" alt="CessAdvisor Logo" aClass="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white" />
-    <p class="my-6 text-gray-500 dark:text-gray-400">CessAdvisor, open source toilet review system.</p>
-    <FooterLinkGroup ulClass="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
-      <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/">Home</FooterLink>
-      <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/toilets">Toilets</FooterLink>
-      <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/reviews">Reviews</FooterLink>
-      <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/profile">Profile</FooterLink>
-      <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/login">Login Program</FooterLink>
-    </FooterLinkGroup>
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">I'm open source, under TBD licence</span>
-  </div>
-</Footer>
+<h2>FOoter</h2>
--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,15 +1,1 @@
-<script>
-  import { Section, Page404 } from 'flowbite-svelte-blocks';
-  import { Button } from 'flowbite-svelte';
-</script>
-
-<Section name="page404">
-  <Page404>
-    <svelte:fragment slot="h1">404</svelte:fragment>
-    <svelte:fragment slot="paragraph">
-      <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Not found.</p>
-      <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page.</p>
-      <Button href="/" size="lg" color="red">Back to Homepage</Button>
-    </svelte:fragment>
-  </Page404>
-</Section>
+<h1>Not found</h1>
