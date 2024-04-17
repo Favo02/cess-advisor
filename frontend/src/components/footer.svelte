29afad0d433fc8f775b3c4ea05dac47453a515bb
Luca Favini <luca.favini.02@gmail.com>
2024-04-17 22:50:03 +0200

    feat(fe/components): add `footer` and `header`

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,1 +1,20 @@
-<h2>FOoter</h2>
+<script>
+  import Icon from '@iconify/svelte';
+</script>
+
+<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+  <nav class="grid grid-flow-col gap-4">
+    <a href="/" class="link link-hover">Home</a>
+    <a href="/toilets" class="link link-hover">Toilets</a>
+    <a href="/reviews" class="link link-hover">Reviews</a>
+    <a href="/profile" class="link link-hover">Profile</a>
+  </nav>
+  <nav>
+    <div class="grid grid-flow-col gap-4">
+      <a href="/"><img src="/logo.png" alt="CessAdvisor logo" class="h-12"></a>
+    </div>
+  </nav>
+  <aside>
+    <p>CessAdvisor - 2024 - I'm <a href="https://github.com/Favo02/cess-advisor" class="link">open source</a> <Icon icon="fa:external-link-square" class="inline"/>, under TDB license.</p>
+  </aside>
+</footer>
--- frontend/src/components/header.svelte
+++ frontend/src/components/header.svelte
@@ -1,1 +1,26 @@
-<h1>Header</h1>
+<div class="navbar bg-base-100 px-20">
+  <div class="navbar-start">
+    <a href="/"><img src="/logo.png" alt="CessAdvisor logo" class="h-12"></a>
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabindex="-1" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a href="/">Home</a></li>
+        <li><a href="/toilets">Toilets</a></li>
+        <li><a href="/reviews">Reviews</a></li>
+      </ul>
+    </div>
+    <a href="/" class="btn btn-ghost text-xl">CessAdvisor</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/">Home</a></li>
+      <li><a href="/toilets">Toilets</a></li>
+      <li><a href="/reviews">Reviews</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <a href="/profile" class="btn">Profile</a>
+  </div>
+</div>
