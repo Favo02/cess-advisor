cd187cc1ae91062d488b33d9d454252fdf10a907
Luca Favini <luca.favini.02@gmail.com>
2024-04-22 00:48:18 +0200

    feat(fe/pages): add `login` page

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,43 +1,83 @@
 <script>
   import axios from "axios";
 
+  import Icon from "@iconify/svelte";
+
   let username = "";
   let password = "";
   let loading = false;
 
   async function handleSubmit() {
-    try {
-      loading = true;
+    loading = true;
 
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/login`, { username, password });
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/login`,
+        { username, password },
+        { withCredentials: true }
+      );
 
       if (response.status === 200) {
-        alert("Login successful");
+        window.location.href = "/profile";
       } else {
-        alert("Login failed");
+        alert("Error creating review");
       }
+
     } catch (error) {
-      alert("Login failed");
+      alert(`Error: ${error.response.data.error} - ${error.response.data.message}`);
     } finally {
       loading = false;
     }
   }
+
 </script>
 
-<h1>Login</h1>
+<div class="w-full py-28 bg-base-300">
+  <h1 class="mx-auto text-4xl text-center mb-3 font-bold">Welcome, please <span class="text-primary">login</span></h1>
+  <p class="mx-auto text-lg text-center mb-10 italic">Don't have an account? <a href="/register" class="text-primary link">Register</a>.</p>
 
-{#if loading}
-  <p>Loading...</p>
-{:else}
-  <form on:submit|preventDefault={handleSubmit}>
-    <label>
-      Username:
-      <input type="text" bind:value={username} />
-    </label>
-    <label>
-      Password:
-      <input type="password" bind:value={password} />
-    </label>
-    <button type="submit">Submit</button>
+  <form class="max-w-sm mx-auto">
+
+    <div class="pb-6">
+      <label for="ignore" class="block mb-2 ml-2 text-md font-medium text-base-content">Username</label>
+      <div class="relative peer">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+          <Icon icon="clarity:user-solid" class="w-5 h-5" />
+        </div>
+        <input type="text" class="bg-base-100 border border-base-content text-base-content text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5" placeholder="User" bind:value={username}>
+      </div>
+    </div>
+
+    <div class="pb-8">
+      <label for="ignore" class="block mb-2 ml-2 text-md font-medium text-base-content">Password</label>
+      <div class="relative peer">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+          <Icon icon="tabler:key-filled" class="w-5 h-5" />
+        </div>
+        <input type="password" class="bg-base-100 border border-base-content text-base-content text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5" placeholder="P4s$w0rd" bind:value={password}>
+      </div>
+    </div>
+
+    <button class="btn btn-primary mx-auto w-full mb-8" on:click={handleSubmit}>
+      {#if loading}
+        <Icon icon="line-md:loading-loop" class="w-6 h-6" />
+      {:else}
+        <Icon icon="carbon:login" class="w-6 h-6" />
+      {/if}
+      Login
+    </button>
+    <div class="flex">
+      <a href="/register" class="btn btn-primary btn-outline mx-auto w-[calc(50%-8px)] mr-2 mb-8">
+        <Icon icon="mdi:register" class="w-6 h-6" />
+        Register
+      </a>
+      <a href="/" class="btn btn-primary btn-outline mx-auto w-[calc(50%-8px)] ml-2">
+        <Icon icon="pajamas:go-back" class="w-6 h-6" />
+        Homepage
+      </a>
+    </div>
+
   </form>
-{/if}
+
+</div>
+
