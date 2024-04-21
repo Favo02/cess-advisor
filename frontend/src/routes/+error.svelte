f95854dc303855b5f2fcea151b4a031e3edf9a85
Luca Favini <luca.favini.02@gmail.com>
2024-04-22 01:17:58 +0200

    feat(fe/pages): add 404 page

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,1 +1,4 @@
-<h1>Not found</h1>
+<div class="w-full pt-40 bg-base-300 min-h-[calc(100vh-15rem)] flex justify-center align-middle flex-col">
+  <h1 class="mx-auto text-4xl text-center mb-3 font-bold">We're sorry, page <span class="text-primary">not found</span></h1>
+  <p class="mx-auto text-lg text-center mb-10 italic">Error 404, go back to <a href="/home" class="text-primary link">home</a>.</p>
+</div>
