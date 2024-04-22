8d31a8094e4cb1ea042e6ac3be98f6f1f9f38f9f
Luca Favini <luca.favini.02@gmail.com>
2024-04-22 13:54:10 +0200

    feat(fe/pages): add validation hints in `register` page

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -73,7 +73,7 @@
 
     <h1 class="text-justify rounded-md p-8 mb-8 text-black bg-red-400 border-red-950 border-4">WARNING: the app is having some issues with CORS and cookies. If the login don't work, please enable third party cookies in your browser. Sorry for the inconvenience, working on a fix. <a target="_blank" href="https://github.com/Favo02/cess-advisor/issues/9" class="link">Issue on GitHub</a>.</h1>
 
-    <div class="pb-6">
+    <div class="pb-2">
       <label for="ignore" class="block mb-2 ml-2 text-md font-medium text-base-content">Username</label>
       <div class="relative peer">
         <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
@@ -81,9 +81,10 @@
         </div>
         <input type="text" class="bg-base-100 border border-base-content text-base-content text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5" placeholder="User" bind:value={username}>
       </div>
+      <p class="text-xs m-2 text-center opacity-60 invisible peer-focus-within:visible">4 - 16 characters, alphanumeric characters only (a-z, A-Z, 0-9)</p>
     </div>
 
-    <div class="pb-6">
+    <div>
       <label for="ignore" class="block mb-2 ml-2 text-md font-medium text-base-content">Password</label>
       <div class="relative peer">
         <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
@@ -91,9 +92,10 @@
         </div>
         <input type="password" class="bg-base-100 border border-base-content text-base-content text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5" placeholder="P4s$w0rd" bind:value={password}>
       </div>
+      <p class="text-xs m-2 text-center opacity-60 invisible peer-focus-within:visible">8 - 20 characters, at least one letter (a-z, A-Z), one digit (0-9), one special character (@$!%*#?&)</p>
     </div>
 
-    <div class="pb-8">
+    <div class="pb-3">
       <label for="ignore" class="block mb-2 ml-2 text-md font-medium text-base-content">Confirm password</label>
       <div class="relative peer">
         <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
@@ -101,6 +103,7 @@
         </div>
         <input type="password" class="bg-base-100 border border-base-content text-base-content text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5" placeholder="P4s$w0rd" bind:value={confirmPassword}>
       </div>
+      <p class="text-xs m-2 text-center opacity-60 invisible peer-focus-within:visible">The same password you typed above</p>
     </div>
 
     <button class="btn btn-primary mx-auto w-full mb-8" on:click={handleSubmit}>
