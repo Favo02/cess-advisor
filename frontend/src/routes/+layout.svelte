15bfefc3d18e7479cc51e7b25fab97c876c07d4b
Luca Favini <luca.favini.02@gmail.com>
2024-04-22 02:22:51 +0200

    build(fe): add `svelte-french-toast` notifications library

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,9 +3,13 @@
 
   import Header from "../components/header.svelte";
   import Footer from "../components/footer.svelte";
+
+  import {Toaster} from "svelte-french-toast"
 </script>
 
 <Header />
+
+<Toaster />
 
 <div class="">
   <slot />
