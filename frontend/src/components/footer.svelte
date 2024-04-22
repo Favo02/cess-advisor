43127cfc391bf7dd3819e259c0d2ce2f0ee48104
Luca Favini <luca.favini.02@gmail.com>
2024-04-23 01:15:22 +0200

    fix(fe/components): add missing `_black` to links and fix missing links

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -19,15 +19,15 @@
   <aside>
     <p class="text-md">
       <span>
-        I'm <a href="https://github.com/Favo02/cess-advisor" class="link text-primary">open source <Icon icon="fa:external-link-square" class="inline w-3 h-3 text-primary"/></a>,
+        I'm <a href="https://github.com/Favo02/cess-advisor" target="_blank" class="link text-primary">open source <Icon icon="fa:external-link-square" class="inline w-3 h-3 text-primary"/></a>,
       </span>
       <span>
-        under <a href="https://creativecommons.org/licenses/by/4.0/" class="link text-primary">CC-BY 4.0 <Icon icon="fa:external-link-square" class="inline w-3 h-3 text-primary"/></a> license.
+        under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html#license-text" target="_blank" class="link text-primary">GPLv3 <Icon icon="fa:external-link-square" class="inline w-3 h-3 text-primary"/></a> license.
       </span>
     </p>
     <p class="text-base-content/70">
-      <a href="https://github.com/Favo02/cess-advisor/pulls" class="link">Contributions are welcome <Icon icon="fa:external-link-square" class="inline w-3 h-3"/></a>.
-      <a href="https://github.com/Favo02/cess-advisor/issues" class="link">Report a bug <Icon icon="fa:external-link-square" class="inline w-3 h-3"/></a>.
+      <a href="https://github.com/Favo02/cess-advisor/pulls" target="_blank" class="link">Contributions are welcome <Icon icon="fa:external-link-square" class="inline w-3 h-3"/></a>.
+      <a href="https://github.com/Favo02/cess-advisor/issues" target="_blank" class="link">Report a bug <Icon icon="fa:external-link-square" class="inline w-3 h-3"/></a>.
     </p>
   </aside>
 </footer>
