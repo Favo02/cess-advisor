0d9c41da1dfb7d07aee9058c53a8e27f30d0b890
Luca Favini <luca.favini.02@gmail.com>
2024-04-22 03:06:02 +0200

    feat(fe/utils): add regexes and schemas for validation

---
+++ frontend/src/utils/schemas.js
@@ -0,0 +1,32 @@
+import { z } from "zod"
+import regex from "./regex"
+
+const login = z.object({
+  username: z.string().regex(regex.username),
+  password: z.string().regex(regex.password),
+})
+
+const review = z.object({
+  toilet: z.string().uuid(),
+  rating: z.number().int().min(0).max(10),
+  description: z.string().regex(regex.genericLongText),
+  paper: z.boolean(),
+  soap: z.boolean(),
+  dryer: z.boolean(),
+  hotwater: z.boolean(),
+  clean: z.number().int().min(0).max(10),
+  temperature: z.number().int().min(0).max(10),
+})
+
+const toilet = z.object({
+  title: z.string().regex(regex.genericShortText),
+  building: z.string().regex(regex.genericShortText),
+  place: z.string().regex(regex.genericShortText),
+  description: z.string().regex(regex.genericLongText),
+})
+
+export default {
+  login,
+  review,
+  toilet,
+}
